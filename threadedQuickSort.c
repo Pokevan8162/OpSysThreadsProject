52fc8ba7b21c99b6cb445c7edacd59aa4b97048e
Pokevan8162 <163201695+Pokevan8162@users.noreply.github.com>
2025-02-10 15:21:34 -0500

    Update threadedQuickSort.c

--- threadedQuickSort.c
+++ threadedQuickSort.c
@@ -11,7 +11,7 @@ typedef struct {
 } indices;
 
 int main(int argc, char *argv[]) {
-	nt arr[ARRAY_SIZE];
+    int arr[ARRAY_SIZE];
     srand(time(NULL));
 
 	for (int i = 0; i < ARRAY_SIZE; i++) {
