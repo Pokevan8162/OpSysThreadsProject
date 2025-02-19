908833bbcdd1f018d8ee570e12c2dcfa19662a03
Pokevan8162 <163201695+Pokevan8162@users.noreply.github.com>
2025-02-19 16:19:18 -0500

    finish threadedMergeSort.c

--- threadedMergeSort.c
+++ threadedMergeSort.c
@@ -1,125 +1,117 @@
+// Average computation time: 0.0010178 seconds (10 test runs, 0.0006932 seconds faster on average than merge sort)
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <time.h>
-#include <math.h>
-
 #define ARRAY_SIZE 10000
-
-void *runner(void *param);
-void *merge(int arr[], int left, int mid, int right);
-void *mergeSort(int arr[], int left, int right);
+#define THREAD_COUNT 4
 
 typedef struct {
-	int first;
-	int last;
+    int left;
+    int right;
 } indices;
 
-int main(int argc, char *argv[]) {
-	int arr[ARRAY_SIZE];
-    	srand(time(NULL));
-
-    	// Fill the array with random integers
-    	for (int i = 0; i < ARRAY_SIZE; i++) {
-        	arr[i] = rand() % 100000;
-    	}
-	
-	indices *set1 = (indices *) malloc(sizeof(indices));
-	set1->first = 0;
-	set1->last = (int) ARRAY_SIZE/4;
-	
-	indices *set2 = (indeces *) malloc(sizeof(indices));
-	set2->first = (int) ARRAY_SIZE/4 + 1;
-	set2->last = (int) ARRAY_SIZE/2;
-	
-	indices *set3 = (indices *) malloc(sizeof(indices));
-	set3->first = (int) ARRAY_SIZE/2 + 1;
-	set3->last = (int) ARRAY_SIZE*0.75;
-	
-	indices *set4 = (indices *) malloc(sizeof(indices));
-	set4->first = (int) ARRAY_SIZE*0.75 + 1;
-	set4->last = ARRAY_SIZE - 1;
-	
-
-	pthread_t thread0, thread1, thread2, thread3;
-	pthread_create(&thread0, NULL, mergesort, set1);
-	pthread_create(&thread1, NULL, mergesort, set2);
-	pthread_create(&thread2, NULL, mergesort, set3);
-	pthread_create(&thread3, NULL, mergesort, set4);
-	
-	pthread_join(thread0, NULL);
-	pthread_join(thread1, NULL);
-	pthread_join(thread2, NULL);
-	pthread_join(thread3, NULL);
-}
-
-//Runner
-void *runner(void *param) {
-	indices *set = (indices *)param;
-	int n = set->last - set->first + 1; 
-    	
-      	// Sorting array using mergesort
-    	mergeSort(arr, set->first, set->last);
-	pthread_exit(0);	
-}
+int arr[ARRAY_SIZE];
 
 void merge(int arr[], int left, int mid, int right) {
     int i, j, k;
     int n1 = mid - left + 1;
     int n2 = right - mid;
 
-    // Create temporary arrays
     int leftArr[n1], rightArr[n2];
 
-    // Copy data to temporary arrays
     for (i = 0; i < n1; i++)
         leftArr[i] = arr[left + i];
     for (j = 0; j < n2; j++)
         rightArr[j] = arr[mid + 1 + j];
 
-    // Merge the temporary arrays back into arr[left..right]
-    i = 0;
-    j = 0;
+    i = 0; 
+    j = 0; 
     k = left;
+    
     while (i < n1 && j < n2) {
         if (leftArr[i] <= rightArr[j]) {
-            arr[k] = leftArr[i];
-            i++;
+            arr[k++] = leftArr[i++];
+        } else {
+            arr[k++] = rightArr[j++];
         }
-        else {
-            arr[k] = rightArr[j];
-            j++;
-        }
-        k++;
     }
 
-    // Copy the remaining elements of leftArr[], if any
-    while (i < n1) {
-        arr[k] = leftArr[i];
-        i++;
-        k++;
-    }
+    while (i < n1) arr[k++] = leftArr[i++];
+    while (j < n2) arr[k++] = rightArr[j++];
+}
 
-    // Copy the remaining elements of rightArr[], if any
-    while (j < n2) {
-        arr[k] = rightArr[j];
-        j++;
-        k++;
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
     }
 }
 
-// The subarray to be sorted is in the index range [left-right]
-void mergeSort(int arr[], int left, int right) {
-    if (left < right) {
-      
-        // Calculate the midpoint
-        int mid = left + (right - left) / 2;
+void* mergeSortThread(void* arg) { // What the threads use for their specific array indices
+    indices *args = (indices*)arg;
+    mergeSort(arr, args->left, args->right);
+    pthread_exit(NULL);
+}
 
-        // Sort first and second halves
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
+void* mergeThread(void* arg) { // What the threads use to merge the arrays back together
+    indices *args = (indices*)arg;
+    int left = args->left;
+    int right = args->right;
+    int mid = left + (right - left) / 2;
 
-        // Merge the sorted halves
-        merge(arr, left, mid, right);
+    merge(arr, left, mid, right);
+    pthread_exit(NULL);
+}
+
+int main() {
+    pthread_t threads[THREAD_COUNT];
+    indices threadIndices[THREAD_COUNT]; // Carries the indices per thread
+
+    srand(time(NULL));
+    for (int i = 0; i < ARRAY_SIZE; i++) { 
+        arr[i] = rand() % 100000;
     }
+
+    int partitionSize = ARRAY_SIZE / THREAD_COUNT;
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
+    for (int i = 0; i < THREAD_COUNT; i++) { // Creates threads and puts them in the merge sort function
+        threadIndices[i].left = i * partitionSize;
+        threadIndices[i].right = (i == THREAD_COUNT - 1) ? (ARRAY_SIZE - 1) : ((i + 1) * partitionSize - 1);
+        pthread_create(&threads[i], NULL, mergeSortThread, &threadIndices[i]);
+    }
+
+    for (int i = 0; i < THREAD_COUNT; i++) { // Waits for the threads to finish sorting
+        pthread_join(threads[i], NULL);
+    }
+
+    int mergePass = THREAD_COUNT; // Limit on how many merges are allowed to happen at once, up to how many threads we define it as
+    while (mergePass > 1) { // While there are still arrays that need to be merged
+        int mergeThreadCount = mergePass / 2; // We wont need all 4 threads to merge 4 subarrays (for ex), we'd only need two, so divide it by 2
+        pthread_t mergeThreads[mergeThreadCount];
+        indices mergeData[mergeThreadCount];
+
+        for (int i = 0; i < mergeThreadCount; i++) {
+            mergeData[i].left = threadIndices[i * 2].left;
+            mergeData[i].right = threadIndices[i * 2 + 1].right;
+
+            pthread_create(&mergeThreads[i], NULL, mergeThread, &mergeData[i]);
+        }
+
+        for (int i = 0; i < mergeThreadCount; i++) {
+            pthread_join(mergeThreads[i], NULL);
+        }
+
+        mergePass /= 2;
+    }
+
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    double elapsed = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+    printf("Elapsed time: %f seconds\n", elapsed);
+
+    return 0;
 }
