5fb019aa5733e1caf7caf6ccc0141505f92d8869
Pokevan8162 <163201695+Pokevan8162@users.noreply.github.com>
2025-02-19 12:35:20 -0500

    Update mergeSort.c
    make merge sort intentionally single threaded

--- mergeSort.c
+++ mergeSort.c
@@ -1,93 +1,82 @@
-// C program for the implementation of merge sort
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <time.h>
-#include <math.h>
+
 #define ARRAY_SIZE 10000
 
 // Merges two subarrays of arr[].
-// First subarray is arr[left..mid]
-// Second subarray is arr[mid+1..right]
 void merge(int arr[], int left, int mid, int right) {
     int i, j, k;
     int n1 = mid - left + 1;
     int n2 = right - mid;
 
-    // Create temporary arrays
     int leftArr[n1], rightArr[n2];
 
-    // Copy data to temporary arrays
     for (i = 0; i < n1; i++)
         leftArr[i] = arr[left + i];
     for (j = 0; j < n2; j++)
         rightArr[j] = arr[mid + 1 + j];
 
-    // Merge the temporary arrays back into arr[left..right]
     i = 0;
     j = 0;
     k = left;
     while (i < n1 && j < n2) {
         if (leftArr[i] <= rightArr[j]) {
-            arr[k] = leftArr[i];
-            i++;
+            arr[k++] = leftArr[i++];
+        } else {
+            arr[k++] = rightArr[j++];
         }
-        else {
-            arr[k] = rightArr[j];
-            j++;
-        }
-        k++;
     }
 
-    // Copy the remaining elements of leftArr[], if any
-    while (i < n1) {
-        arr[k] = leftArr[i];
-        i++;
-        k++;
-    }
-
-    // Copy the remaining elements of rightArr[], if any
-    while (j < n2) {
-        arr[k] = rightArr[j];
-        j++;
-        k++;
-    }
+    while (i < n1) arr[k++] = leftArr[i++];
+    while (j < n2) arr[k++] = rightArr[j++];
 }
 
-// The subarray to be sorted is in the index range [left-right]
+// Merge sort function
 void mergeSort(int arr[], int left, int right) {
     if (left < right) {
-      
-        // Calculate the midpoint
         int mid = left + (right - left) / 2;
-
-        // Sort first and second halves
         mergeSort(arr, left, mid);
         mergeSort(arr, mid + 1, right);
-
-        // Merge the sorted halves
         merge(arr, left, mid, right);
     }
 }
 
-int main(int argc, char *argv[]) {
+void* runMergeSort(void* arg) {
+    struct timespec start, end;
     int arr[ARRAY_SIZE];
     srand(time(NULL));
 
-	for (int i = 0; i < ARRAY_SIZE; i++) {
+    for (int i = 0; i < ARRAY_SIZE; i++) {
         arr[i] = rand() % 100000;
     }
-    
-    printf("First 10 elements of the array:\n");
-	for (int i = 0; i < 10; i++) {
+
+    printf("First 10 elements before sorting:\n");
+    for (int i = 0; i < 10; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
-    int n = sizeof(arr) / sizeof(arr[0]);    
-      // Sorting arr using mergesort
-    mergeSort(arr, 0, n - 1);
 
-    for (int i = 0; i < n; i++)
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    mergeSort(arr, 0, ARRAY_SIZE - 1);
+    clock_gettime(CLOCK_MONOTONIC, &end);
+
+    double elapsed = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+    printf("Elapsed time: %f seconds\n", elapsed);
+
+    printf("First 10 elements after sorting:\n");
+    for (int i = 0; i < 10; i++) {
         printf("%d ", arr[i]);
+    }
+    printf("\n");
+    
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    pthread_t singleThread;
+    pthread_create(&singleThread, NULL, runMergeSort, NULL);
+    pthread_join(singleThread, NULL);
     return 0;
 }
